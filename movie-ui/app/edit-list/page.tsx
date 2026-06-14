69401fffee6bb4bf541d041fc84fac68458f3217
chrissaia <144486134+chrissaia@users.noreply.github.com>
2026-06-14 16:36:12 -0400

    profile addition

--- movie-ui/app/edit-list/page.tsx
+++ movie-ui/app/edit-list/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { useEffect, useMemo, useState, kindFromUrl } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+
+import AuthProfileButton from "../components/AuthProfileButton";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8000";
@@ -975,13 +977,7 @@ export default function EditListsPage() {
               Search
             </button>
 
-            {!isSignedIn && (
-              <SignInButton mode="modal">
-                <button className="primary-btn">Sign In</button>
-              </SignInButton>
-            )}
-
-            {isSignedIn && <UserButton />}
+            <AuthProfileButton />
           </div>
         </div>
 
